f332f951cd0e35d38282255d70a6922cb1fe9c0e
rangaimal <rangaimal1234@gmail.com>
2018-03-01 11:57:20 +0530

    array functions minimum value & count

---
+++ if.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>check the marks</title>
+</head>
+<body>
+<?php 
+$marks=60;
+If($marks>=75){
+	echo "Grade:A";
+}else if($marks>=65) {
+	echo "Grade:B";
+}else if($marks>=50){
+	echo "Grade:C";
+}else{
+	echo "you are not qualified";
+}
+?>
+
+
+</body>
+</html>
