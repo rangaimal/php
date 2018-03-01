3d3755221cf43897127e6e6fea6a1c25d4ee7f0f
rangaimal <rangaimal1234@gmail.com>
2018-03-01 11:11:24 +0530

    array functions maximum value

---
+++ arrayfunction.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Array function</title>
+</head>
+<body>
+<?php 
+$mynumbers=array(12,12,45,67,45,32);?>
+
+Maximum number:<?php echo max($mynumbers);?>
+
+
+
+</body>
+</html>
